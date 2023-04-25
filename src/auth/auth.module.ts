97e1bdfe5120c84a675f8e699d0bc800738d05b1
Jchan <gkswocks123@gmail.com>
2023-04-25 17:24:31 +0900

    Feat : auth controller 생성

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { KakaoModule } from './kakao/kakao.module';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [KakaoModule],
+  controllers: [AuthController],
 })
 export class AuthModule {}
