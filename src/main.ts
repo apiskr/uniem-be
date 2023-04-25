0d53064abeff0d56822840e97e645a7bf1173cc8
Jchan <gkswocks123@gmail.com>
2023-04-25 16:24:08 +0900

    Feat : PORT 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 4000;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      // origin: process.env.ORIGIN_URI,
+      credentials: true,
+    },
+  });
+  await app.listen(PORT);
+  console.log(`http://localhost:${PORT}/`);
 }
 bootstrap();
