3544a9217f6ce4b2b5aa565c7d5d2ba96302ced1
Jchan <gkswocks123@gmail.com>
2023-04-27 14:29:38 +0900

    Feat : ResGetUserInfo type 추가

--- src/api/kakaoApi.ts
+++ src/api/kakaoApi.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { baseURLs } from 'src/constants/baseUrls';
 
 const ROUTE = '/v2/user/me';
@@ -7,7 +7,39 @@ const kakaoApiInstance = axios.create({
   baseURL: baseURLs.KAKAO_API,
 });
 
-const getUserInfo = async (accessToken: string) => {
+type ResGetUserInfo = {
+  id: number;
+  connected_at: string;
+  properties: {
+    nickname: string;
+    profile_image: string;
+    thumbnail_image: string;
+  };
+  kakao_account: {
+    profile_needs_agreement: boolean;
+    profile: {
+      nickname: string;
+      thumbnail_image_url: string;
+      profile_image_url: string;
+    };
+    has_email: boolean;
+    email_needs_agreement: boolean;
+    is_email_valid: boolean;
+    is_email_verified: boolean;
+    email: string;
+    has_age_range: boolean;
+    age_range_needs_agreement: boolean;
+    age_range: string;
+    has_birthday: boolean;
+    birthday_needs_agreement: boolean;
+    birthday: string;
+    birthday_type: string;
+  };
+};
+
+const getUserInfo = async (
+  accessToken: string,
+): Promise<AxiosResponse<ResGetUserInfo>> => {
   return await kakaoApiInstance.get(`${ROUTE}`, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
