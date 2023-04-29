d126a0c4d2ef4b06bd8dfc11a2f78596cf7559c8
Jchan <gkswocks123@gmail.com>
2023-04-30 00:44:46 +0900

    Feat : user 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { ConfigurationModule } from './configuration/configuration.module';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, ConfigurationModule],
+  imports: [AuthModule, ConfigurationModule, DatabaseModule, UserModule],
   controllers: [AppController],
 })
 export class AppModule {}
